fd0b47b32887ab307955e84ff4006cf6f9d4ef1f
jiaming <erichu666@126.com>
2022-03-23 04:51:54 +0000

    modify: modification of file names

--- packetloss_simulation/lstm/output_prediction_step/pathgen.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-
-_pa = pathlib.Path(__file__).parent
-for n in [250]:
-	os.system(f'mkdir {_pa}/{n}ahead/Generalisation')
-	os.system(f'mkdir {_pa}/{n}ahead/test_magnified')
-	os.system(f'mkdir {_pa}/{n}ahead/test')
-
